ed436b6e2bfeaef377183fa45f8bc9b9c10bdfa4
Agustin Amicone <amiconeagustin@gmail.com>
2025-12-14 21:10:26 -0300

    cambio de bootstrapapp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
